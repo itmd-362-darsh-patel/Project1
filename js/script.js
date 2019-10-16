9aa93f258cc5c1ef3fe8b60e2d32dff1ab137579
pateldarsh14 <43825079+pateldarsh14@users.noreply.github.com>
2019-10-16 18:05:56 -0500

    added javascript

--- js/script.js
+++ js/script.js
@@ -5,3 +5,9 @@ if (phone.length < 10) {
   alert("Phone number needs to be 10 digits");
   submitOk = "false";
 }
+
+function AlertPage() {
+  alert("Thanks! You can now learn Filmmaking!");
+
+  return false;	
+}
