8ee2ce7c1ed29de9c3027ae03822621ddf0eb00f
pateldarsh14 <43825079+pateldarsh14@users.noreply.github.com>
2019-10-23 23:46:12 -0500

    added js as async

--- js/script.js
+++ js/script.js
@@ -1,13 +1,5 @@
-var phone = document.getElementById("phone_num").value;
-submitOk = "true";
-
-if (phone.length < 10) {
-  alert("Phone number needs to be 10 digits");
-  submitOk = "false";
-}
-
 function AlertPage() {
   alert("Thanks! You can now learn Filmmaking!");
-
+  
   return false;	
 }
